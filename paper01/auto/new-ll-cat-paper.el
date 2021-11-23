afe2a4e90e044cfc94ac4569a244ab472be00d80
William Henney <will@henney.org>
2021-11-23 11:18:34 -0600

    Small changes to figure caption

--- paper01/auto/new-ll-cat-paper.el
+++ paper01/auto/new-ll-cat-paper.el
@@ -5,13 +5,13 @@
                      '(("aastex63" "apj" "twocolumn")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("newtxmath" "varg") ("babel" "spanish" "es-minimal" "english") ("inputenc" "utf8")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -40,7 +40,6 @@
     "babel"
     "inputenc"
     "natbib"
-    "microtype"
     "hyperref"
     "savesym"
     "siunitx")
